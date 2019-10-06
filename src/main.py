c326791e3f0f1ca1b24324316db409652f43ec90
Zef Delgadillo <zef@google.com>
2019-10-06 17:18:22 -0400

    Add licenses

--- src/main.py
+++ src/main.py
@@ -1,4 +1,16 @@
-# Copyright 2019 Google LLC. This software is provided as-is, without warranty or representation for any use or purpose. Your use of it is subject to your agreement with Google.  
+# Copyright 2019 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 from google.cloud import storage, bigquery
 import base64
@@ -7,9 +19,7 @@ import os
 import re
 from flask import escape
 from datetime import datetime
-import warnings
 
-warnings.filterwarnings("ignore", "Your application has authenticated using end user credentials")
 fixity_date = datetime.now()
 
 def main(bucket={}, event={}):
